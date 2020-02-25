08fc86e56cdf1b143134496aa7aa3c45f41a7553
Kiran Kumar Aitha <SLS\kiran.aitha@l0494d.sls.ads.valuelabs.net>
2020-02-25 17:55:37 +0530

    updated the sql queries which used

--- hospital_db.sql
+++ hospital_db.sql
@@ -137,3 +137,19 @@ ALTER TABLE `patients`
   ADD CONSTRAINT `patients_ibfk_1` FOREIGN KEY (`doctor_id`) REFERENCES `doctors` (`id`),
   ADD CONSTRAINT `patients_ibfk_2` FOREIGN KEY (`nurse_id`) REFERENCES `nurses` (`id`);
 COMMIT;
+
+
+
+
+PATIENT TABLE CREATION:
+
+create table patients(id int(11) auto_increment primary key,name varchar(255)UNIQUE NOT NULL,email varchar(255)UNIQUE NOT NULL,phone int(15)UNIQUE NOT NULL,gender int(11)NOT NULL,health_issue varchar(255)NOT NULL,assigned_doctor varchar(255),foreign key(assigned_doctor) references doctors(name),assigned_nurse varchar(255) UNIQUE,foreign key(assigned_nurse) references nurses(name),created_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP);
+
+alter table patients add discharged_flag int(11);
+
+
+
+
+
+
+
